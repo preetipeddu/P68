a984ba4aa74055723fdfae2311bf822e6333dc20
preetipeddu <peddupreeti@gmail.com>
2021-07-05 09:59:49 -0400

    created two tabs for Instagram and Facebook

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
+import FacebookScreen from './FacebookScreen';
+import InstagramScreen from './InstagramScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <AppContainer/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TabNavigator = createBottomTabNavigator({
+  Facebook: {screen: FacebookScreen},
+  Instagram: {screen: InstagramScreen}
+})
+
+const AppContainer = createAppContainer(TabNavigator);
+
+
